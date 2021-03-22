1f3f0aed32c64c2704d0757ab3df6455ab7f00e3
Kevin D Goveas <64493268+kevkanae@users.noreply.github.com>
2021-03-22 09:37:58 +0530

    sample case passed but WA

--- Google Kickstart/RecordBreaker/rb.cpp
+++ Google Kickstart/RecordBreaker/rb.cpp
@@ -10,14 +10,34 @@ int main()
     for (int i = 0; i < T; i++)
     {
         int count = 0;
+        int prev = 0;
         std::cin >> N;
         int arr[N];
         for (int j = 0; j < N; j++)
         {
             std::cin >> arr[j];
         }
-        for (int k = 0; k < N; k++)
+        if (arr[0] > arr[1])
         {
+            count++;
+        }
+        for (int k = 1; k < N; k++)
+        {
+            if (k != N - 1)
+            {
+                if (arr[k] > arr[k - 1] && arr[k] > arr[k + 1] && arr[k] > prev)
+                {
+                    count++;
+                    prev = arr[k];
+                }
+            }
+            else
+            {
+                if (arr[k] > arr[k - 1] && arr[k] > prev)
+                {
+                    count++;
+                }
+            }
         }
         std::cout << "Case #" << i + 1 << ": " << count << "\n";
     }
